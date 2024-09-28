b74fe969f6659280ddcb756288c78850d8a0176c
Hyun-hyo-min <hyomin2677@gmail.com>
2024-09-28 21:34:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/subwate_backend/SubwateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.subwate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubwateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/subwate_backend/SubwateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.subwate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
