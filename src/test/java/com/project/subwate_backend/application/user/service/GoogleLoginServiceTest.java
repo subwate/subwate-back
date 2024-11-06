7fac050333013b65f4119527f563db73dbc106ca
elephant97 <elephant0908@gmail.com>
2024-11-06 21:23:10 +0900

    SUB-26 feat: 사용자는 구글아이디를 통해 로그인할 수 있다
    * google 콜백 controller 테스트 추가
    * GoogleLoginServiceTest 추가하여 구글 로그인 과정 및 가입되지 않은 사용자 응답 테스트 추가

---
+++ src/test/java/com/project/subwate_backend/application/user/service/GoogleLoginServiceTest.java
@@ -0,0 +1,103 @@
+package com.project.subwate_backend.application.user.service;
+
+import com.project.subwate_backend.application.exception.UnregisteredUserException;
+import com.project.subwate_backend.common.mapper.ResponseMapper;
+import com.project.subwate_backend.domain.user.entity.User;
+import com.project.subwate_backend.infrastructure.google.service.GoogleApiService;
+import com.project.subwate_backend.infrastructure.security.JwtTokenProvider;
+import com.project.subwate_backend.infrastructure.user.repository.UserRepository;
+import com.project.subwate_backend.presentation.user.dto.response.UserLoginDto;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@DisplayName("구글 로그인 서비스 테스트")
+class GoogleLoginServiceTest {
+    private GoogleLoginService googleLoginService;
+
+    @MockBean
+    private GoogleApiService googleApiService;
+
+    @MockBean
+    private ResponseMapper responseMapper;
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private UserLoginDto mockUserLoginDto;
+
+    @BeforeEach
+    void setUp() {
+        googleLoginService = new GoogleLoginService(googleApiService, userRepository, jwtTokenProvider, responseMapper);
+
+        mockUserLoginDto = new UserLoginDto();
+        mockUserLoginDto.setEmail("test@example.com");
+        mockUserLoginDto.setName("Test User");
+        mockUserLoginDto.setNickname("TestNickname");
+        mockUserLoginDto.setSocialLoginInfo("google");
+    }
+
+    @Test
+    @DisplayName("[구글 로그인]회원가입되지 않은 사용자 구글 로그인 시 가입되지 않은 사용자 예외 발생")
+    void givenUnregisterUser_whenGoogleLogin_thenThrowUnregisteredUserException() {
+        //given
+        String code = "mockCode";
+
+        when(googleApiService.getAccessToken(code)).thenReturn("mockAccessToken");
+        when(googleApiService.getUserInfo(anyString())).thenReturn(mockUserLoginDto);
+
+        //when
+        UnregisteredUserException exception = assertThrows(UnregisteredUserException.class, () -> {
+            googleLoginService.login(code);
+        });
+
+        //then
+        assertEquals("가입되지 않은 사용자입니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("[구글 로그인]회원가입 된 사용자 구글 로그인 시 유저 정보 및 토큰 발행")
+    void givenRegisteredUser_whenGoogleLogin_ThenReturnUserInfo() {
+        //given
+        String code = "mockCode";
+        User mockUser = User.from(mockUserLoginDto);
+
+        userRepository.save(mockUser);
+
+        UserLoginDto expectedResponseDto = new UserLoginDto();
+        expectedResponseDto.setEmail("test@google.com");
+        expectedResponseDto.setAccessToken("mockAccessToken");
+        expectedResponseDto.setRefreshToken("mockRefreshToken");
+
+
+        when(googleApiService.getAccessToken(anyString())).thenReturn("mockAccessToken");
+        when(googleApiService.getUserInfo(anyString())).thenReturn(mockUserLoginDto);
+        when(responseMapper.toUserLoginDto(mockUser)).thenReturn(expectedResponseDto);
+        when(jwtTokenProvider.createToken(mockUserLoginDto.getEmail(), mockUser.getId())).thenReturn("mockAccessToken");
+        when(jwtTokenProvider.createRefreshToken(mockUserLoginDto.getEmail())).thenReturn("mockRefreshToken");
+
+        //when
+        UserLoginDto result = googleLoginService.login(code);
+
+        // then
+        assertNotNull(result);
+        assertEquals(expectedResponseDto.getEmail(), result.getEmail());
+        assertEquals(expectedResponseDto.getAccessToken(), result.getAccessToken());
+        assertEquals(expectedResponseDto.getRefreshToken(), result.getRefreshToken());
+    }
+}
